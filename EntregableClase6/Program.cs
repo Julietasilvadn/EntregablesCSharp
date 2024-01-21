19fa55379d0deff3cab05e1f46714514ff66c3b2
Julieta Silva <108014946+Julietasilvadn@users.noreply.github.com>
2024-01-21 16:20:13 -0300

    cambio

---
+++ EntregableClase6/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Usuario
+{
+    private int id;
+    private string nombre;
+    private string apellido;
+    private string nombreUsuario;
+    private string contraseña;
+    private string mail;
+}
+
+public class Producto
+{
+    private int id;
+    private string descripcion;
+    private double costo;
+    private double precioVenta;
+    private int stock;
+    private int idUsuario;
+}
+
+public class ProductoVendido
+{
+    private int id;
+    private int idProducto;
+    private int stock;
+    private int idVenta;
+}
+
+public class Venta
+{
+    private int id;
+    private string comentarios;
+    private int idUsuario;
+}
