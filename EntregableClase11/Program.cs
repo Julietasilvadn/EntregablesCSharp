45f472154f2bcdcc249d3c5f142f91723c0bab43
Julieta Silva <108014946+Julietasilvadn@users.noreply.github.com>
2024-02-18 20:53:18 -0300

    EntregableNormalizando

---
+++ EntregableClase11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntregableClase11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            List<Usuario> listado = UsuarioADO.ObtenerTodosLosUsuarios();
+
+
+            Console.WriteLine("Termine");
+
+        }
+    }
+}
